e14611fe2b127e86a25f4acee1a70e22ab47ab39
WafflesNL <seltenbart@gmail.com>
2016-09-07 09:16:36 +0200

    Accountinfo

--- Proftaak forms/Forms version 1.0/Froms/HomeForm.Designer.cs
+++ Proftaak forms/Forms version 1.0/Froms/HomeForm.Designer.cs
@@ -30,24 +30,80 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
             this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnTijdlijn = new System.Windows.Forms.Button();
+            this.cbSelectEvent = new System.Windows.Forms.ComboBox();
+            this.btnBetalingStatus = new System.Windows.Forms.Button();
+            this.btnEventInfo = new System.Windows.Forms.Button();
+            this.btnAccount = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLogOut
             // 
-            this.btnLogOut.Location = new System.Drawing.Point(12, 411);
+            this.btnLogOut.Location = new System.Drawing.Point(9, 334);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(90, 44);
+            this.btnLogOut.Size = new System.Drawing.Size(68, 36);
             this.btnLogOut.TabIndex = 0;
             this.btnLogOut.Text = "Log out";
             this.btnLogOut.UseVisualStyleBackColor = true;
             // 
+            // btnTijdlijn
+            // 
+            this.btnTijdlijn.Location = new System.Drawing.Point(12, 46);
+            this.btnTijdlijn.Name = "btnTijdlijn";
+            this.btnTijdlijn.Size = new System.Drawing.Size(129, 28);
+            this.btnTijdlijn.TabIndex = 1;
+            this.btnTijdlijn.Text = "Naar Tijdlijn";
+            this.btnTijdlijn.UseVisualStyleBackColor = true;
+            // 
+            // cbSelectEvent
+            // 
+            this.cbSelectEvent.FormattingEnabled = true;
+            this.cbSelectEvent.Location = new System.Drawing.Point(12, 17);
+            this.cbSelectEvent.Name = "cbSelectEvent";
+            this.cbSelectEvent.Size = new System.Drawing.Size(283, 21);
+            this.cbSelectEvent.TabIndex = 2;
+            // 
+            // btnBetalingStatus
+            // 
+            this.btnBetalingStatus.Location = new System.Drawing.Point(166, 46);
+            this.btnBetalingStatus.Name = "btnBetalingStatus";
+            this.btnBetalingStatus.Size = new System.Drawing.Size(129, 28);
+            this.btnBetalingStatus.TabIndex = 3;
+            this.btnBetalingStatus.Text = "Betalingsstatus";
+            this.btnBetalingStatus.UseVisualStyleBackColor = true;
+            // 
+            // btnEventInfo
+            // 
+            this.btnEventInfo.Location = new System.Drawing.Point(12, 92);
+            this.btnEventInfo.Name = "btnEventInfo";
+            this.btnEventInfo.Size = new System.Drawing.Size(129, 28);
+            this.btnEventInfo.TabIndex = 4;
+            this.btnEventInfo.Text = "Eventinformatie";
+            this.btnEventInfo.UseVisualStyleBackColor = true;
+            // 
+            // btnAccount
+            // 
+            this.btnAccount.Location = new System.Drawing.Point(166, 334);
+            this.btnAccount.Name = "btnAccount";
+            this.btnAccount.Size = new System.Drawing.Size(129, 36);
+            this.btnAccount.TabIndex = 5;
+            this.btnAccount.Text = "Accountinformatie";
+            this.btnAccount.UseVisualStyleBackColor = true;
+            // 
             // HomeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 467);
+            this.ClientSize = new System.Drawing.Size(307, 379);
+            this.Controls.Add(this.btnAccount);
+            this.Controls.Add(this.btnEventInfo);
+            this.Controls.Add(this.btnBetalingStatus);
+            this.Controls.Add(this.cbSelectEvent);
+            this.Controls.Add(this.btnTijdlijn);
             this.Controls.Add(this.btnLogOut);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "HomeForm";
             this.Text = "HomeForm";
             this.ResumeLayout(false);
@@ -57,5 +113,10 @@
         #endregion
 
         private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnTijdlijn;
+        private System.Windows.Forms.ComboBox cbSelectEvent;
+        private System.Windows.Forms.Button btnBetalingStatus;
+        private System.Windows.Forms.Button btnEventInfo;
+        private System.Windows.Forms.Button btnAccount;
     }
 }
--- Proftaak forms/Forms version 1.0/Froms/LoginForm.Designer.cs
+++ Proftaak forms/Forms version 1.0/Froms/LoginForm.Designer.cs
@@ -41,23 +41,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(187, 248);
+            this.textBox1.Location = new System.Drawing.Point(140, 202);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 22);
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(187, 276);
+            this.textBox2.Location = new System.Drawing.Point(140, 224);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(259, 22);
+            this.textBox2.Size = new System.Drawing.Size(195, 20);
             this.textBox2.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(253, 304);
+            this.btnLogin.Location = new System.Drawing.Point(190, 247);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(122, 50);
+            this.btnLogin.Size = new System.Drawing.Size(92, 41);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -65,27 +68,31 @@
             // lblGebruikersNaam
             // 
             this.lblGebruikersNaam.AutoSize = true;
-            this.lblGebruikersNaam.Location = new System.Drawing.Point(52, 248);
+            this.lblGebruikersNaam.Location = new System.Drawing.Point(39, 202);
+            this.lblGebruikersNaam.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblGebruikersNaam.Name = "lblGebruikersNaam";
-            this.lblGebruikersNaam.Size = new System.Drawing.Size(117, 17);
+            this.lblGebruikersNaam.Size = new System.Drawing.Size(87, 13);
             this.lblGebruikersNaam.TabIndex = 3;
             this.lblGebruikersNaam.Text = "Gebruikersnaam:";
             // 
             // lblWachtwoord
             // 
             this.lblWachtwoord.AutoSize = true;
-            this.lblWachtwoord.Location = new System.Drawing.Point(52, 276);
+            this.lblWachtwoord.Location = new System.Drawing.Point(39, 224);
+            this.lblWachtwoord.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblWachtwoord.Name = "lblWachtwoord";
-            this.lblWachtwoord.Size = new System.Drawing.Size(90, 17);
+            this.lblWachtwoord.Size = new System.Drawing.Size(71, 13);
             this.lblWachtwoord.TabIndex = 4;
             this.lblWachtwoord.Text = "Wachtwoord:";
             // 
             // pb4Events
             // 
             this.pb4Events.Image = global::Forms_version_1._0.Properties.Resources._4Events;
-            this.pb4Events.Location = new System.Drawing.Point(187, 12);
+            this.pb4Events.Location = new System.Drawing.Point(107, 12);
+            this.pb4Events.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pb4Events.Name = "pb4Events";
-            this.pb4Events.Size = new System.Drawing.Size(259, 191);
+            this.pb4Events.Size = new System.Drawing.Size(263, 163);
+            this.pb4Events.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb4Events.TabIndex = 5;
             this.pb4Events.TabStop = false;
             // 
@@ -93,17 +100,18 @@
             // 
             this.lblInlogstatus.AutoSize = true;
             this.lblInlogstatus.ForeColor = System.Drawing.Color.Red;
-            this.lblInlogstatus.Location = new System.Drawing.Point(184, 218);
+            this.lblInlogstatus.Location = new System.Drawing.Point(138, 177);
+            this.lblInlogstatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblInlogstatus.Name = "lblInlogstatus";
-            this.lblInlogstatus.Size = new System.Drawing.Size(283, 17);
+            this.lblInlogstatus.Size = new System.Drawing.Size(212, 13);
             this.lblInlogstatus.TabIndex = 6;
             this.lblInlogstatus.Text = "Het inloggen is mislukt probeer het opnieuw";
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 366);
+            this.ClientSize = new System.Drawing.Size(485, 297);
             this.Controls.Add(this.lblInlogstatus);
             this.Controls.Add(this.pb4Events);
             this.Controls.Add(this.lblWachtwoord);
@@ -112,6 +120,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormLogin";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pb4Events)).EndInit();

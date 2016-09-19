250089648d17d7320e5be59d86b21dd510051964
WafflesNL <seltenbart@gmail.com>
2016-09-19 12:15:16 +0200

    Filter Mignon

---
+++ Proftaak forms/Forms version 1.0/Forms/Filter.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Forms_version_1._0.Forms
+{
+    partial class Filter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNaam = new System.Windows.Forms.Label();
+            this.lblWoord = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnZoek = new System.Windows.Forms.Button();
+            this.btnTerug = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblNaam
+            // 
+            this.lblNaam.AutoSize = true;
+            this.lblNaam.Location = new System.Drawing.Point(12, 9);
+            this.lblNaam.Name = "lblNaam";
+            this.lblNaam.Size = new System.Drawing.Size(38, 13);
+            this.lblNaam.TabIndex = 0;
+            this.lblNaam.Text = "Naam:";
+            // 
+            // lblWoord
+            // 
+            this.lblWoord.AutoSize = true;
+            this.lblWoord.Location = new System.Drawing.Point(12, 37);
+            this.lblWoord.Name = "lblWoord";
+            this.lblWoord.Size = new System.Drawing.Size(42, 13);
+            this.lblWoord.TabIndex = 1;
+            this.lblWoord.Text = "Woord:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Categorie";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnZoek
+            // 
+            this.btnZoek.Location = new System.Drawing.Point(185, 93);
+            this.btnZoek.Name = "btnZoek";
+            this.btnZoek.Size = new System.Drawing.Size(75, 23);
+            this.btnZoek.TabIndex = 6;
+            this.btnZoek.Text = "Zoek";
+            this.btnZoek.UseVisualStyleBackColor = true;
+            // 
+            // btnTerug
+            // 
+            this.btnTerug.Location = new System.Drawing.Point(12, 93);
+            this.btnTerug.Name = "btnTerug";
+            this.btnTerug.Size = new System.Drawing.Size(75, 23);
+            this.btnTerug.TabIndex = 7;
+            this.btnTerug.Text = "Terug";
+            this.btnTerug.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(76, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(184, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Filter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 126);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnTerug);
+            this.Controls.Add(this.btnZoek);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblWoord);
+            this.Controls.Add(this.lblNaam);
+            this.Name = "Filter";
+            this.Text = "Filter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNaam;
+        private System.Windows.Forms.Label lblWoord;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnZoek;
+        private System.Windows.Forms.Button btnTerug;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}

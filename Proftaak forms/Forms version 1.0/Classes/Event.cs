8b33334b9c5fc194b824a9c5c8c4e220b13779b6
WafflesNL <seltenbart@gmail.com>
2016-10-03 10:33:00 +0200

    Implementatie

---
+++ Proftaak forms/Forms version 1.0/Classes/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms_version_1._0.Classes
+{
+    public class Event
+    {
+        public List<Materiaal> Materiaallist = new List<Materiaal>();
+        public Account Account { get; set; }
+        public Tijdlijn Tijdlijn { get; set; }
+        public string Naam { get; set; }
+        public List<Reservering> Reserveringslist = new List<Reservering>();
+        public Event(List<Reservering> reserveringslist, Account account, List<Materiaal> materiaallist)
+        {
+            this.Reserveringslist = reserveringslist;
+            this.Account = account;
+            this.Materiaallist = materiaallist;
+
+        }
+
+        public void EditEvent()
+        {
+            
+        }
+    }
+}
---
+++ Proftaak forms/Forms version 1.0/Classes/Tijdlijn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms_version_1._0.Classes
+{
+    public class Tijdlijn
+    {
+        public string Eventname { get; set; }
+        List<Post> Postlist = new List<Post>();
+        public Event Event { get; set; }
+
+        public Tijdlijn(string eventname)
+        {
+            this.Eventname = eventname;
+        }
+
+        public void AddPost(Post post)
+        {
+
+        }
+    }
+}

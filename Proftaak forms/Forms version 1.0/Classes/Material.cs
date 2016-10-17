c2387410e12ebe8dbfe85f473a14bffd11d36fab
Jordi <j.vandermeij@student.fontys.nl>
2016-10-17 13:30:59 +0200

    Prepared Material

--- Proftaak forms/Forms version 1.0/Classes/Material.cs
+++ Proftaak forms/Forms version 1.0/Classes/Material.cs
@@ -8,15 +8,38 @@ namespace Forms_version_1._0.Classes
 {
     public class Material
     {
+        string type;
+        public string Type { get; }
+
         public int Price { get; set; }
      
         public string Name { get; set; }
 
-        // material stuff
         public Material(int price, string name)
         {
             this.Price = price;         
             this.Name = name;
+        }
+
+        public void GetAccount()
+        {
+
+        }
+
+        public void Rent()
+
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public int CalculatePrice()
+        {
+            throw new NotImplementedException();
         }
     }
 }

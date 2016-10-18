fe7db18f4f78a3b610a3f5ae61be6d5904f2d23e
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-10-18 18:36:55 +0200

    Getmaterial toegevoegd

---
+++ Proftaak forms/Forms version 1.0/Classes/Database/MaterialManagement/DatabaseGetMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms_version_1._0
+{
+    public static class DatabaseGetMaterial
+    {
+        //needs to get all material en put them in a list
+    }
+}

35e06140012701facd92322c74a9ba3b8e49f030
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-10-21 10:46:43 +0200

    sfdfdf

---
+++ Proftaak forms/Forms version 1.0/Forms/EventManagementForms/MateriaalEventForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Forms_version_1._0
+{
+    partial class MateriaalEventForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbMaterialAvailable = new System.Windows.Forms.ListBox();
+            this.lbMaterialForEvent = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.lbMaterialAvailable);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 384);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Materiaal beschikbaar";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.btnRemove);
+            this.groupBox2.Controls.Add(this.lbMaterialForEvent);
+            this.groupBox2.Location = new System.Drawing.Point(534, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(503, 384);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "materiaal voor event";
+            // 
+            // lbMaterialAvailable
+            // 
+            this.lbMaterialAvailable.FormattingEnabled = true;
+            this.lbMaterialAvailable.ItemHeight = 16;
+            this.lbMaterialAvailable.Location = new System.Drawing.Point(6, 32);
+            this.lbMaterialAvailable.Name = "lbMaterialAvailable";
+            this.lbMaterialAvailable.Size = new System.Drawing.Size(491, 292);
+            this.lbMaterialAvailable.TabIndex = 0;
+            // 
+            // lbMaterialForEvent
+            // 
+            this.lbMaterialForEvent.FormattingEnabled = true;
+            this.lbMaterialForEvent.ItemHeight = 16;
+            this.lbMaterialForEvent.Location = new System.Drawing.Point(6, 32);
+            this.lbMaterialForEvent.Name = "lbMaterialForEvent";
+            this.lbMaterialForEvent.Size = new System.Drawing.Size(491, 292);
+            this.lbMaterialForEvent.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(393, 330);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(104, 41);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Naar event";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(6, 330);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(104, 41);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Verwijder";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(393, 330);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(104, 41);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Opslaan";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // MateriaalEventForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 405);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MateriaalEventForm";
+            this.Text = "MateriaalEventForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbMaterialAvailable;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lbMaterialForEvent;
+    }
+}
---
+++ Proftaak forms/Forms version 1.0/Forms/EventManagementForms/MateriaalEventForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms_version_1._0
+{
+    public partial class MateriaalEventForm : Form
+    {
+        public MateriaalEventForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

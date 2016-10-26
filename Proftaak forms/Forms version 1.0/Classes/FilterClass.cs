1ade05a4dd11142e970f6ae1a342a738f492121a
WafflesNL <seltenbart@gmail.com>
2016-10-26 15:27:52 +0200

    Fml

---
+++ Proftaak forms/Forms version 1.0/Classes/FilterClass.cs
@@ -0,0 +1,58 @@
+﻿using Forms_version_1._0.Classes.Database.TimelineManagement;
+using Forms_version_1._0.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms_version_1._0.Classes
+{
+    public class FilterClass
+    {
+        public List<Post> Filterlist = new List<Post>();
+        public FilterClass()
+        {
+                    
+        }
+
+        public List<Post> GetAll(string naam, string woord, string categorie)
+        {
+            return Filterlist = DatabaseGetPost.GetAll(naam, woord, categorie);
+        }
+        public List<Post> GetName()
+        {
+            return null;
+           // return Filterlist = DatabaseGetPost.GetName();
+        }
+
+        public List<Post> GetWord()
+        {
+            return null;
+            // return Filterlist = DatabaseGetPost.GetWord();
+        }
+
+        public List<Post> GetCategory()
+        {
+            return null;
+            // return Filterlist = DatabaseGetPost.GetCategory();
+        }
+
+        public List<Post> GetNameWord()
+        {
+            return null;
+            // return Filterlist = DatabaseGetPost.GetNameWord();
+        }
+
+        public List<Post> GetNameCategory()
+        {
+            return null;
+            // return Filterlist = DatabaseGetPost.GetNameCategory();
+        }
+        public List<Post> GetWordCategory()
+        {
+            return null;
+            // return Filterlist = DatabaseGetPost.GetWordCategory();
+        }
+    }
+}

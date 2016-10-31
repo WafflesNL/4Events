14c1155b6c5294703f5563aa69330b812d38ecea
WafflesNL <seltenbart@gmail.com>
2016-10-31 12:49:37 +0100

    Unittests Mediasharing Bugfixing Mediasharing

--- Proftaak forms/Forms version 1.0/Forms/EventManagementForms/MateriaalEventForm.Designer.cs
+++ Proftaak forms/Forms version 1.0/Forms/EventManagementForms/MateriaalEventForm.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lbMaterialAvailable = new System.Windows.Forms.ListBox();
-            this.lbMaterialForEvent = new System.Windows.Forms.ListBox();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.btnRemove = new System.Windows.Forms.Button();
+            this.lbMaterialAvailable = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnSave = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lbMaterialForEvent = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -43,80 +43,88 @@
             // 
             this.groupBox1.Controls.Add(this.btnAdd);
             this.groupBox1.Controls.Add(this.lbMaterialAvailable);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(503, 384);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(377, 312);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Materiaal beschikbaar";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(295, 268);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(78, 33);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Naar event";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbMaterialAvailable
+            // 
+            this.lbMaterialAvailable.FormattingEnabled = true;
+            this.lbMaterialAvailable.Location = new System.Drawing.Point(4, 26);
+            this.lbMaterialAvailable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbMaterialAvailable.Name = "lbMaterialAvailable";
+            this.lbMaterialAvailable.Size = new System.Drawing.Size(369, 238);
+            this.lbMaterialAvailable.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.btnSave);
             this.groupBox2.Controls.Add(this.btnRemove);
             this.groupBox2.Controls.Add(this.lbMaterialForEvent);
-            this.groupBox2.Location = new System.Drawing.Point(534, 12);
+            this.groupBox2.Location = new System.Drawing.Point(400, 10);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(503, 384);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(377, 312);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "materiaal voor event";
-            // 
-            // lbMaterialAvailable
-            // 
-            this.lbMaterialAvailable.FormattingEnabled = true;
-            this.lbMaterialAvailable.ItemHeight = 16;
-            this.lbMaterialAvailable.Location = new System.Drawing.Point(6, 32);
-            this.lbMaterialAvailable.Name = "lbMaterialAvailable";
-            this.lbMaterialAvailable.Size = new System.Drawing.Size(491, 292);
-            this.lbMaterialAvailable.TabIndex = 0;
-            // 
-            // lbMaterialForEvent
-            // 
-            this.lbMaterialForEvent.FormattingEnabled = true;
-            this.lbMaterialForEvent.ItemHeight = 16;
-            this.lbMaterialForEvent.Location = new System.Drawing.Point(6, 32);
-            this.lbMaterialForEvent.Name = "lbMaterialForEvent";
-            this.lbMaterialForEvent.Size = new System.Drawing.Size(491, 292);
-            this.lbMaterialForEvent.TabIndex = 1;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(393, 330);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(104, 41);
-            this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "Naar event";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnRemove
-            // 
-            this.btnRemove.Location = new System.Drawing.Point(6, 330);
-            this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(104, 41);
-            this.btnRemove.TabIndex = 2;
-            this.btnRemove.Text = "Verwijder";
-            this.btnRemove.UseVisualStyleBackColor = true;
-            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            this.groupBox2.Text = "Materiaal voor event";
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(393, 330);
+            this.btnSave.Location = new System.Drawing.Point(295, 268);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(104, 41);
+            this.btnSave.Size = new System.Drawing.Size(78, 33);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Opslaan";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(4, 268);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(78, 33);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Verwijder";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lbMaterialForEvent
+            // 
+            this.lbMaterialForEvent.FormattingEnabled = true;
+            this.lbMaterialForEvent.Location = new System.Drawing.Point(4, 26);
+            this.lbMaterialForEvent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbMaterialForEvent.Name = "lbMaterialForEvent";
+            this.lbMaterialForEvent.Size = new System.Drawing.Size(369, 238);
+            this.lbMaterialForEvent.TabIndex = 1;
+            // 
             // MateriaalEventForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1049, 405);
+            this.ClientSize = new System.Drawing.Size(787, 329);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MateriaalEventForm";
             this.Text = "MateriaalEventForm";
             this.groupBox1.ResumeLayout(false);

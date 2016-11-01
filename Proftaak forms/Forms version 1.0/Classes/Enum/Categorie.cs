562033b1efca31ec476a2594b598cf8ce0107e33
Wartenberg,Koen K.W. <k.wartenberg@student.fontys.nl>
2016-11-01 14:03:31 +0100

    fgfgf

--- Proftaak forms/Forms version 1.0/Classes/Enum/Categorie.cs
+++ Proftaak forms/Forms version 1.0/Classes/Enum/Categorie.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Forms_version_1._0
 {
+    /// <summary>
+    /// Different categories for camping places
+    /// </summary>
     public enum Categorie
     {
         Luxe,
